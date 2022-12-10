ad1c2111a4a759ad5c7ba438a2400e3bcba2f94f
elvishsieh <elvis.hsieh@gmail.com>
2022-12-10 11:59:16 +0800

    modified 06filePermission and add two instal oh-my-posh shell

---
+++ teaching/src/bash/installOhmyposh.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+mkdir ~/bin
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O ~/bin/oh-my-posh
+chmod +x ~/bin/oh-my-posh
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm ~/.poshthemes/themes.zip
+
+echo 'eval "$(~/bin/oh-my-posh init bash --config ~/.poshthemes/atomic.omp.json)"' >> ~/.bashrc
---
+++ teaching/src/bash/ohmyposh.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+OMP=oh-my-posh.tgz
+FN=InconsolataLGC.zip
+
+if [[ -f $OMP ]]; then
+    tar zxvf oh-my-posh.tgz -C ~/
+fi
+
+if [[ -f $FN ]]; then
+    unzip InconsolataLGC.zip -d ~/.local/share/fonts
+fi
+
+chmod +x ~/bin/posh-linux-amd64
+
+echo 'eval "$(~/bin/posh-linux-amd64 init bash --config ~/.poshthemes/atomic.omp.json)"' >> ~/.bashrc
+
+rm -i $OMP $FN
